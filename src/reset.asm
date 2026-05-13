1c2e71e7856dd0a5b860110deb24d3edbd4e29e6
moro3264 <183396130+moro3264@users.noreply.github.com>
2026-05-13 13:27:17 +0300

    Altele. Sper ca de acuma sa fie gata setupu la workflow

--- src/reset.asm
+++ src/reset.asm
@@ -3,12 +3,14 @@
 .include "driver_6522.asm"
 .include "driver_6551.asm"
 
+.CODE
+
 RESET:
     SEI                 ;   ==========================================
     CLD                 ;                SECVENTA DE RESET 
-    LDX #$FF            ;   ~ dezactivare intreruperi si mod BCD
-    TXS                 ;   ~ init stiva 
-    LDA #$00            ;   ~ init registre: A = X = Y = 0x00        
+    LDX #$FF            ;   * dezactivare intreruperi si mod BCD
+    TXS                 ;   * init stiva 
+    LDA #$00            ;   * init registre: A = X = Y = 0x00        
     TAX                 ;   
     TAY                 ;   ==========================================
 
@@ -21,7 +23,6 @@ IRQ:
     RTI
 
 .SEGMENT "VECTORI"
-
 .word   NMI
 .word   RESET
 .word   IRQ
